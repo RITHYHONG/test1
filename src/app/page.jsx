04ee7bcb9b7d50862668ebef9d7ee59317e66efe
RITHYHONG <rithyhong8998@gmail.com>
2024-04-08 14:44:04 +0700

    image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ function MyApp() {
     className="bg-cover bg-center h-screen"
     style={{backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRYkN4dTGHnRgPkouUttfIihHYK27DRxll2WPNmMwFVA&s")'}}
   >
-      <img src="" alt="" />
+      <img src="../assets/hudfb.png" alt="" />
     </main>
   );
 }
