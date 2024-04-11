4c666edce92e4d4f7da9e416b99b22a32128f975
RITHYHONG <rithyhong8998@gmail.com>
2024-04-11 22:40:08 +0700

    not yet with star

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,25 @@
 // import AllMovie from '@/components/AllMovie'; // Adjust the path accordingly
 // import ActionMovie from '@/components/ActionMovie'; // Adjust the path accordingly
+import MoviesByGenre from "@/components/MoviesByGenre";
 import GetAllMovie from "@/components/GetAllMovie";
-import ActionMovies from "@/components/ActionMovie";
+// import ActionMovies from "@/components/ActionMovie";
 import { data } from "autoprefixer";
-import AnimeMovies from "@/components/AnimeMovie";
-import DramaMovies from "@/components/DramaMovie";
-import ScienceFictionMovies from "@/components/ScienceFictionMovie";
+// import AnimeMovies from "@/components/AnimeMovie";
+// import DramaMovies from "@/components/DramaMovie";
+// import ScienceFictionMovies from "@/components/ScienceFictionMovie";
 
 const HomePage = () => {
+  const genres = ['Action', 'Anime', 'Drama', 'Science Fiction'];
   // const movies = getAllMovies();
   // const allMovies = movies.payload;
   return (
     <div>
-      <main className="bg-cover bg-center h-screen filter grayscale-50 bg-[url('https://puui.wetvinfo.com/vcover_hz_pic/0/2knhnaakii18oxj1683882661123/0?imageMogr2/thumbnail/600x|imageMogr2/thumbnail/600x')]">
+<main className="bg-cover bg-center h-screen" style={{ backgroundImage: "url('https://puui.wetvinfo.com/vcover_hz_pic/0/2knhnaakii18oxj1683882661123/0?imageMogr2/thumbnail/600x|imageMogr2/thumbnail/600x')", opacity: "0.6" }}>
       </main>
       <GetAllMovie data={data}/>
-      <ScienceFictionMovies data={data}/>
-      <ActionMovies data={data}/>
-      <AnimeMovies data={data}/>
-      <DramaMovies data={data}/>
+      {genres.map((genre) => (
+        <MoviesByGenre key={genre} genre={genre} />
+      ))}
     </div>
   );
 };
--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -10,38 +10,38 @@ const NavbarComponent = () => {
             href="/"
             className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased"
           >
-            <img className="w-40" src="./assets/Legend-Cinema.png" alt="" />
+            <img className="w-40" src="../assets/Legend-Cinema.png" alt="" />
           </Link>
           <div className="flex items-center gap-4">
             <div className="hidden mr-4 lg:block">
               <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-                <li className="block p-1 font-sans text-3xl antialiased font-normal leading-normal text-white hover:bg-indigo-100 hover:text-black py-2 px-3 rounded-lg">
+                <li className="block p-1 font-sans text-xl antialiased font-normal leading-normal text-white hover:bg-indigo-100 hover:text-black py-2 px-3 rounded-lg">
                   <Link href="/" className="flex items-center">
                     Home
                   </Link>
                 </li>
-                <li className="block p-1 font-sans text-3xl antialiased font-normal leading-normal text-white hover:bg-indigo-100 hover:text-black py-2 px-3 rounded-lg">
-                  <Link href="/products" className="flex items-center">
+                <li className="block p-1 font-sans text-xl antialiased font-normal leading-normal text-white hover:bg-indigo-100 hover:text-black py-2 px-3 rounded-lg">
+                  <Link href="/" className="flex items-center">
                     Popular
                   </Link>
                 </li>
-                <li className="block p-1 font-sans text-3xl antialiased font-normal leading-normal text-white hover:bg-indigo-100 hover:text-black py-2 px-3 rounded-lg">
-                  <Link href="/blog" className="flex items-center">
+                <li className="block p-1 font-sans text-xl antialiased font-normal leading-normal text-white hover:bg-indigo-100 hover:text-black py-2 px-3 rounded-lg">
+                  <Link href="/" className="flex items-center">
                     Romence
                   </Link>
                 </li>
-                <li className="block p-1 font-sans text-3xl antialiased font-normal leading-normal text-white hover:bg-indigo-100 hover:text-black py-2 px-3 rounded-lg">
-                  <Link href="/setting" className="flex items-center">
+                <li className="block p-1 font-sans text-xl antialiased font-normal leading-normal text-white hover:bg-indigo-100 hover:text-black py-2 px-3 rounded-lg">
+                  <Link href="/" className="flex items-center">
                     Anime
                   </Link>
                 </li>
               </ul>
             </div>
-            <div className="form-control ">
+            <div className="form-control">
               <input
                 type="text"
                 placeholder="Type to Search"
-                className="input text-xl input-bordered w-24 md:w-auto py-[0.40rem] rounded-md pl-3 bg-white"
+                className="input text-sm input-bordered w-24 md:w-auto py-[0.35rem] rounded-md pl-3 bg-white"
               />
             </div>
           </div>
